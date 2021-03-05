1bdeb6d8566826f4770ea01c4872aafd4e42a798
Lunac <msten75@gmail.com>
2021-03-05 08:44:29 +0100

    Added task components

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,45 @@
 <template>
   <div class="container">
     <Header title="Task tracker" span="world" />
+    <Tasks :tasks="tasks" />
   </div>
 </template>
 
 <script>
 import Header from './components/Header';
-
+import Tasks from './components/Tasks';
 export default {
   name: 'App',
   components: {
     Header,
+    Tasks,
+  },
+  data() {
+    return {
+      tasks: [],
+    };
+  },
+  created() {
+    this.tasks = [
+      {
+        id: 1,
+        text: 'Doctors appointment',
+        day: 'March 1st at 2:30pm',
+        reminder: true,
+      },
+      {
+        id: 2,
+        text: 'Meeting at school',
+        day: 'March 3rd at 1:30pm',
+        reminder: true,
+      },
+      {
+        id: 3,
+        text: 'Food shopping',
+        day: 'March 3rd at 3:30pm',
+        reminder: false,
+      },
+    ];
   },
 };
 </script>
